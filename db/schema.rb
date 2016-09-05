2e4caa1f206bb984eafe6df10123543cdd929574
Matheus e Thiago <matheuscendrao@gmail.com>
2016-09-05 19:15:25 -0300

    starting implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905214602) do
+ActiveRecord::Schema.define(version: 20160905220804) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160905214602) do
     t.string   "location"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "featured"
   end
 
 end
